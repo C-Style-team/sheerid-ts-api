f9202e1604fcf163eb4f5b671a0186f23fadee00
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-11-21 15:57:45 +0900

    chore: refactor union type

--- src/enum-types.ts
+++ src/enum-types.ts
@@ -1,4 +1,7 @@
-export type SheerIDVerificationMethod = "INSTANT" | "MANUAL" | null;
+export type SheerIDVerificationMethod =
+    | null
+    | "INSTANT"
+    | "MANUAL";
 
 export type SheerIDCurrentStep =
     | "success"
@@ -28,145 +31,145 @@ export type SheerIDCurrentStep =
     | "override";
 
 export type SheerIDErrorIds =
-    "internalServerError" |
-    "noProgram" |
-    "invalidProgram" |
-    "expiredProgram" |
-    "inactiveProgram" |
-    "expiredVerification" |
-    "noVerification" |
-    "noOrganization" |
-    "invalidRequest" |
-    "verificationLimitExceeded" |
-    "apiRateLimitExceeded" |
-    "docReviewLimitExceeded" |
-    "noRemainingRewardCodes" |
-    "unknownError" |
-    "invalidApiToken" |
-    "simulatedError" |
-    "invalidDocUploadToken" |
-    "notFound" |
-    "unauthorizedAccountStatus" |
-    "unauthorizedDomain" |
-    "unauthorizedIpAddress" |
-    "unauthorizedRequest" |
-    "unauthorizedUser" |
-    "reverificationDailyLimitExceeded" |
-    "invalidVerificationToken" |
-    "expiredVerificationToken" |
-    "notApproved" |
-    "maxSMSCodeLimitExceeded" |
-    "fraudRulesReject" |
-    "invalidStep" |
-    "invalidOrganization" |
-    "invalidFirstName" |
-    "invalidLastName" |
-    "invalidEmail" |
-    "invalidPhoneNumber" |
-    "invalidBirthDate" |
-    "invalidAddress1" |
-    "invalidCity" |
-    "invalidState" |
-    "invalidPostalCode" |
-    "invalidMilitaryStatus" |
-    "invalidFirstResponderStatus" |
-    "invalidMedicalProfessionalStatus" |
-    "invalidSocialSecurityNumber" |
-    "invalidOverrideCode" |
-    "underagePerson" |
-    "outsideAgePerson" |
-    "futureBirthDate" |
-    "invalidDischargeDate" |
-    "dischargeDateBeforeBirthDate" |
-    "unsupportedDocMimeType" |
-    "invalidFileSizeMax" |
-    "invalidFileSizeEmpty" |
-    "invalidNumberOfFiles" |
-    "maxMetadataValuesExceeded" |
-    "maxMetadataLengthExceeded" |
-    "invalidSMSCode" |
-    "expiredSMSCode" |
-    "incorrectSMSCodeAttemptLimitExceeded" |
-    "incorrectVerificationOverrideCodeAttemptLimitExceeded" |
-    "invalidEmailLoopToken" |
-    "expiredEmailLoopToken";
+    | "internalServerError"
+    | "noProgram"
+    | "invalidProgram"
+    | "expiredProgram"
+    | "inactiveProgram"
+    | "expiredVerification"
+    | "noVerification"
+    | "noOrganization"
+    | "invalidRequest"
+    | "verificationLimitExceeded"
+    | "apiRateLimitExceeded"
+    | "docReviewLimitExceeded"
+    | "noRemainingRewardCodes"
+    | "unknownError"
+    | "invalidApiToken"
+    | "simulatedError"
+    | "invalidDocUploadToken"
+    | "notFound"
+    | "unauthorizedAccountStatus"
+    | "unauthorizedDomain"
+    | "unauthorizedIpAddress"
+    | "unauthorizedRequest"
+    | "unauthorizedUser"
+    | "reverificationDailyLimitExceeded"
+    | "invalidVerificationToken"
+    | "expiredVerificationToken"
+    | "notApproved"
+    | "maxSMSCodeLimitExceeded"
+    | "fraudRulesReject"
+    | "invalidStep"
+    | "invalidOrganization"
+    | "invalidFirstName"
+    | "invalidLastName"
+    | "invalidEmail"
+    | "invalidPhoneNumber"
+    | "invalidBirthDate"
+    | "invalidAddress1"
+    | "invalidCity"
+    | "invalidState"
+    | "invalidPostalCode"
+    | "invalidMilitaryStatus"
+    | "invalidFirstResponderStatus"
+    | "invalidMedicalProfessionalStatus"
+    | "invalidSocialSecurityNumber"
+    | "invalidOverrideCode"
+    | "underagePerson"
+    | "outsideAgePerson"
+    | "futureBirthDate"
+    | "invalidDischargeDate"
+    | "dischargeDateBeforeBirthDate"
+    | "unsupportedDocMimeType"
+    | "invalidFileSizeMax"
+    | "invalidFileSizeEmpty"
+    | "invalidNumberOfFiles"
+    | "maxMetadataValuesExceeded"
+    | "maxMetadataLengthExceeded"
+    | "invalidSMSCode"
+    | "expiredSMSCode"
+    | "incorrectSMSCodeAttemptLimitExceeded"
+    | "incorrectVerificationOverrideCodeAttemptLimitExceeded"
+    | "invalidEmailLoopToken"
+    | "expiredEmailLoopToken";
 
 export type SheerIDSegment =
-    "student" |
-    "teacher" |
-    "military" |
-    "senior" |
-    "age" |
-    "firstResponder" |
-    "medical" |
-    "employment" |
-    "marketplace" |
-    "member" |
-    "licensedProfessional" |
-    "recentMover" |
-    "other";
+    | "student"
+    | "teacher"
+    | "military"
+    | "senior"
+    | "age"
+    | "firstResponder"
+    | "medical"
+    | "employment"
+    | "marketplace"
+    | "member"
+    | "licensedProfessional"
+    | "recentMover"
+    | "other";
 
 export type SheerIDSubSegment =
-    "activeDuty" |
-    "veteran" |
-    "retiree" |
-    "reservist" |
-    "militaryFamily" |
-    "goldStarFamily" |
-    "police" |
-    "fireFighter" |
-    "searchAndRescue" |
-    "emt" |
-    "employee" |
-    "homeBuyer" |
-    "otherMover" |
-    "facultyHighSchool" |
-    "facultyUniversity" |
-    "facultyPostSecondary" |
-    "facultyK12" |
-    "fullAndPartTimeUniversity" |
-    "collegeBound" |
-    "highSchool" |
-    "postSecondary" |
-    "nurse" |
-    "doctor" |
-    "otherHealthWorker" |
-    "dentist" |
-    "pharmacist" |
-    "generalContractor" |
-    "snapBenefits" |
-    "otherGovernmentAssistance" |
-    "architect" |
-    "interiorDesigner" |
-    "librarian" |
-    "childCareWorker" |
-    "veterinarian" |
-    "licensedRealEstateAgent" |
-    "licensedCosmetologist";
+    | "activeDuty"
+    | "veteran"
+    | "retiree"
+    | "reservist"
+    | "militaryFamily"
+    | "goldStarFamily"
+    | "police"
+    | "fireFighter"
+    | "searchAndRescue"
+    | "emt"
+    | "employee"
+    | "homeBuyer"
+    | "otherMover"
+    | "facultyHighSchool"
+    | "facultyUniversity"
+    | "facultyPostSecondary"
+    | "facultyK12"
+    | "fullAndPartTimeUniversity"
+    | "collegeBound"
+    | "highSchool"
+    | "postSecondary"
+    | "nurse"
+    | "doctor"
+    | "otherHealthWorker"
+    | "dentist"
+    | "pharmacist"
+    | "generalContractor"
+    | "snapBenefits"
+    | "otherGovernmentAssistance"
+    | "architect"
+    | "interiorDesigner"
+    | "librarian"
+    | "childCareWorker"
+    | "veterinarian"
+    | "licensedRealEstateAgent"
+    | "licensedCosmetologist";
 
 export type SheerIDOrganizationType =
-    "UNIVERSITY" |
-    "POST_SECONDARY" |
-    "MEMBERSHIP" |
-    "MILITARY" |
-    "FIRST_RESPONDER" |
-    "MEDICAL" |
-    "NON_PROFIT" |
-    "CORPORATE" |
-    "K12" |
-    "AGE_ID" |
-    "HIGH_SCHOOL" |
-    "NONE";
+    | "UNIVERSITY"
+    | "POST_SECONDARY"
+    | "MEMBERSHIP"
+    | "MILITARY"
+    | "FIRST_RESPONDER"
+    | "MEDICAL"
+    | "NON_PROFIT"
+    | "CORPORATE"
+    | "K12"
+    | "AGE_ID"
+    | "HIGH_SCHOOL"
+    | "NONE";
 
 export type SheerIDErrorMessage =
-    `No verification found with id '${string}'` |
-    "Provided API token is empty or invalid" |
-    `Program with id '${string}' is not currently active` |
-    `Verification with ID '${string}' not found` |
-    "User has exceeded the maximum number of verifications" |
-    "User has exceeded the maximum number of document reviews" |
-    `Program with ID '${string}' not found` |
-    `No reward codes remaining for program with ID '${string}'`;
+    | `No verification found with id '${string}'`
+    | "Provided API token is empty or invalid"
+    | `Program with id '${string}' is not currently active`
+    | `Verification with ID '${string}' not found`
+    | "User has exceeded the maximum number of verifications"
+    | "User has exceeded the maximum number of document reviews"
+    | `Program with ID '${string}' not found`
+    | `No reward codes remaining for program with ID '${string}'`;
 
 
 export type SheerIDDocUploadRejectionReason =
