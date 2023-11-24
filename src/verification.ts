ffaa8510954b6f7c9c216e2d4d5f310705bc9e78
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-11-24 21:51:21 +0900

    feat: /verification endpoint

--- src/verification.ts
+++ src/verification.ts
@@ -4,24 +4,51 @@ import { SheerIDResponse, SheerIDSuccessResponse } from "./response-types";
 import { SheerIDVerificationMethod } from "./enum-types";
 import { SheerIDRequest } from "./request";
 
-export class SheerIDVerification extends SheerID {
-    verificationId: string;
+type Ids = {
+    verificationId: string | undefined,
+    programId: string | undefined,
+    trackingId: string | undefined
+};
 
-    constructor(apiToken: string, verificationId: string) {
+export class SheerIDVerification extends SheerID {
+    verificationId: string | undefined;
+    programId: string | undefined;
+    trackingId: string | undefined;
+
+    constructor(apiToken: string, { verificationId, programId, trackingId }: Ids) {
         super(apiToken);
         this.verificationId = verificationId;
+        this.programId = programId;
+        this.trackingId = trackingId;
     }
 
     public status(): Promise<SheerIDSuccessResponse> {
         return new SheerIDRequest<SheerIDSuccessResponse>()
             .endpoint(`/verification/${this.verificationId}`)
+            .set("method", "GET")
+            .send();
+    }
+
+    public statusByTrackingId(): Promise<SheerIDSuccessResponse> {
+        return new SheerIDRequest<SheerIDSuccessResponse>()
+            .endpoint(`/verification/program/${this.programId}/trackingId/${this.trackingId}`)
+            .set("method", "GET")
+            .send();
+    }
+
+    public statusesByTrackingId(): Promise<Array<SheerIDSuccessResponse>> {
+        return new SheerIDRequest<Array<SheerIDSuccessResponse>>()
+            .endpoint(`/verification/program/${this.programId}/trackingId/${this.trackingId}/history`)
+            .set("method", "GET")
+            .set("headers", { "Authorization": `Bearer ${this.apiToken}` })
             .send();
     }
 
     public details(): Promise<SheerIDVerificationStatusDetailsResponse> {
         return new SheerIDRequest<SheerIDVerificationStatusDetailsResponse>()
             .endpoint(`/verification/${this.verificationId}/details`)
-            .set("Authorization", `Bearer ${this.apiToken}`)
+            .set("method", "GET")
+            .set("headers", { "Authorization": `Bearer ${this.apiToken}` })
             .send();
     }
 }
