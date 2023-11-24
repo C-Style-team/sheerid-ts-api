20e85c0e75e20c61ec3f426638a54a47f4fb7d09
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-11-24 16:25:48 +0900

    feat: add function to request to SheerID API

---
+++ src/request.ts
@@ -0,0 +1,34 @@
+export class SheerIDRequest<T> {
+    private m_baseUrl: string = "https://services.sheerid.com/rest/v2";
+    private m_params: Map<string, any> = new Map();
+    private m_endpoint: `/${string}` = "/info";
+
+    constructor() {
+        this.set("Content-Type", "application/json")
+    }
+
+    private canonicalizedParams(): object {
+        let result: any = {};
+        this.m_params.forEach((value, key) => {
+            if (typeof value !== 'undefined') result[key] = value;
+        })
+
+        return result;
+    }
+
+    public set(key: string, value: any): void {
+        if (typeof value !== 'undefined') {
+            this.m_params.set(key, value);
+        }
+    }
+
+    public endpoint(endpoint: `/${string}`): this {
+        this.m_endpoint = endpoint;
+        return this;
+    }
+
+    public async send(): Promise<T> {
+        const response = await fetch(this.m_baseUrl + this.m_endpoint, this.canonicalizedParams());
+        return response.json();
+    }
+}
