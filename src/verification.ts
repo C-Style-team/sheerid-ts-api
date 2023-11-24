e468610c971673191d834b882e6b2293be039b38
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-11-24 16:26:11 +0900

    feat: add verification class

--- src/verification.ts
+++ src/verification.ts
@@ -1,8 +1,27 @@
 import { SheerID } from "./base";
-import {
-    SheerIDVerificationStatus,
-    SheerIDVerificationStatusDetails,
-    SheerIDVerificationMethod,
-} from "./types";
+import { SheerIDVerificationStatusDetailsResponse } from "./types";
+import { SheerIDResponse, SheerIDSuccessResponse } from "./response-types";
+import { SheerIDVerificationMethod } from "./enum-types";
+import { SheerIDRequest } from "./request";
 
+export class SheerIDVerification extends SheerID {
+    verificationId: string;
 
+    constructor(apiToken: string, verificationId: string) {
+        super(apiToken);
+        this.verificationId = verificationId;
+    }
+
+    public status(): Promise<SheerIDSuccessResponse> {
+        return new SheerIDRequest<SheerIDSuccessResponse>()
+            .endpoint(`/verification/${this.verificationId}`)
+            .send();
+    }
+
+    public details(): Promise<SheerIDVerificationStatusDetailsResponse> {
+        return new SheerIDRequest<SheerIDVerificationStatusDetailsResponse>()
+            .endpoint(`/verification/${this.verificationId}/details`)
+            .set("Authorization", `Bearer ${this.apiToken}`)
+            .send();
+    }
+}
