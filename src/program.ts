2458477b3966da7e039b01d6679fb5a0cfda5f37
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-11-26 02:27:44 +0900

    feat: add part of /program endpoint

---
+++ src/program.ts
@@ -0,0 +1,34 @@
+import { SheerID } from "./base";
+import { SheerIDRequest } from "./request";
+import { SheerIDVerificationStep } from "./types/enum"
+import { SheerIDOrganization } from "./types/person";
+
+export class SheerIDProgram {
+    programId: string;
+    constructor(programId: string) {
+        this.programId = programId;
+    }
+
+    // response をかくのがだるい
+    public getProgramTheme() {
+        return new SheerIDRequest<>()
+            .endpoint(`/program/${this.programId}/theme`)
+            .send();
+    }
+
+    public getProgramPossibleSteps() {
+        return new SheerIDRequest<SheerIDVerificationStep>()
+            .endpoint(`/program/${this.programId}/possibleSteps`)
+            .send();
+    }
+
+    /**
+     * @deprecated This endpoint has been deprecated, please use orgSearchUrl in ProgramVerificationConfig instead.
+     */
+    public getOrganizaion(name: string, country?: string) {
+        return new SheerIDRequest<SheerIDOrganization>()
+            .endpoint(`/program/${this.programId}/organization`)
+            .set("body", { name, country })
+            .send();
+    }
+}
