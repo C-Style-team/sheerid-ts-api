35776b26ced6e33e7a19b86a2aedc1ad36407561
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-12-02 02:46:05 +0900

    feat(program): all endpoint

--- src/program.ts
+++ src/program.ts
@@ -2,7 +2,7 @@ import { SheerID } from "./base";
 import { SheerIDRequest } from "./request";
 import { SheerIDVerificationStep } from "./types/enum"
 import { SheerIDOrganization } from "./types/info";
-import { SheerIDErrorResponse, SheerIDProgramThemeResponse } from "./types/response";
+import { SheerIDErrorResponse, SheerIDProgramThemeResponse, SheerIDResponse } from "./types/response";
 
 export class SheerIDProgram extends SheerID {
     programId: string;
@@ -15,12 +15,14 @@ export class SheerIDProgram extends SheerID {
     public getProgramTheme() {
         return new SheerIDRequest<SheerIDProgramThemeResponse | SheerIDErrorResponse>()
             .endpoint(`/program/${this.programId}/theme`)
+            .set("method", "GET")
             .send();
     }
 
     public getProgramPossibleSteps() {
         return new SheerIDRequest<SheerIDVerificationStep | SheerIDErrorResponse>()
             .endpoint(`/program/${this.programId}/possibleSteps`)
+            .set("method", "GET")
             .send();
     }
 
@@ -30,7 +32,46 @@ export class SheerIDProgram extends SheerID {
     public getOrganizaion(name: string, country?: string) {
         return new SheerIDRequest<SheerIDOrganization | SheerIDErrorResponse>()
             .endpoint(`/program/${this.programId}/organization`)
+            .set("method", "GET")
             .set("body", { name, country })
+            .send();
+    }
+
+    public establishVerificationLimit(quantity: number, days: number) {
+        return new SheerIDRequest<{} | SheerIDErrorResponse>()
+            .endpoint(`/program/${this.programId}/verificationLimit`)
+            .set("method", "POST")
+            .set("body", { quantity, days })
+            .send();
+    }
+
+    public deleteVefificationLimit() {
+        return new SheerIDRequest<{} | SheerIDErrorResponse>()
+            .endpoint(`/program/${this.programId}/verificationLimit`)
+            .set("method", "DELETE")
+            .send();
+    }
+
+    public createVerificationWebhook(callbackUri: string) {
+        return new SheerIDRequest<{} | SheerIDErrorResponse>()
+            .endpoint(`/program/${this.programId}/webhook`)
+            .set("method", "POST")
+            .set("body", { callbackUri })
+            .send();
+    }
+
+    public deleteVerificationWebhook() {
+        return new SheerIDRequest<{} | SheerIDErrorResponse>()
+            .endpoint(`/program/${this.programId}/webhook`)
+            .set("method", "DELETE")
+            .send();
+    }
+
+    public sendLink(phoneNumber: string, externalUserId?: string) {
+        return new SheerIDRequest<{} | SheerIDErrorResponse>()
+            .endpoint(`/program/${this.programId}/sendLink`)
+            .set("method", "POST")
+            .set("body", { phoneNumber, externalUserId })
             .send();
     }
 }
